37d7bb3578fc31bad48aa9ca41c28115fb07cc9b
davydov <davydovvip@gmail.com>
2022-12-28 21:05:47 +0300

    refactor: remove redundant

--- hw15-Executors/src/main/java/ru/otus/executor/App.java
+++ hw15-Executors/src/main/java/ru/otus/executor/App.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 
 public class App {
 
@@ -16,29 +17,40 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
     private boolean toggle;
 
+    private final Object lock = new Object();
+
     public static void main(String[] args) {
         new App().demo();
     }
 
     private void demo() {
         var t1 = new Thread(() -> {
-            int value = 0;
+            AtomicInteger value = new AtomicInteger(0);
             while (true) {
-                if (value < 10) {
-                    value = increaseTo(value, 10, true);
+                if (value.get() < 10) {
+                    while (value.get() < 10) {
+                        execInLock(value, AtomicInteger::incrementAndGet, true);
+                    }
                 } else {
-                    value = decreaseTo(value, 0, true);
+                    while (value.get() > 1) {
+                        execInLock(value, AtomicInteger::decrementAndGet, true);
+                    }
                 }
             }
         });
 
         var t2 = new Thread(() -> {
-            int value = 0;
+            AtomicInteger value = new AtomicInteger(0);
             while (true) {
-                if (value < 10) {
-                    value = increaseTo(value, 10, false);
+                if (value.get() < 10) {
+                    while (value.get() < 10) {
+                        execInLock(value, AtomicInteger::incrementAndGet, false);
+                    }
+
                 } else {
-                    value = decreaseTo(value, 0, false);
+                    while (value.get() > 1) {
+                        execInLock(value, AtomicInteger::decrementAndGet, false);
+                    }
                 }
             }
         });
@@ -47,90 +59,22 @@ public class App {
 
         t1.start();
         t2.start();
-
     }
 
-    public int increaseTo(int current, int increaseTo, boolean toggleValue) {
-        var result = new AtomicInteger(current);
-        while (result.get() < increaseTo) {
-            if (toggleValue) {
-                increaseTrueLock(result);
-            } else {
-                increaseFalseLock(result);
+    public void execInLock(AtomicInteger value, Consumer<AtomicInteger> consumer, boolean localToggle) {
+        synchronized (lock) {
+            while (toggle == localToggle) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
+                }
             }
+            toggle = localToggle;
+            lock.notifyAll();
+            logger.info("Value: " + value);
+            consumer.accept(value);
         }
-
-        return result.get();
-    }
-
-    public int decreaseTo(int current, int decreaseTo, boolean toggleValue) {
-        var result = new AtomicInteger(current);
-        while (result.get() > decreaseTo) {
-            if (toggleValue) {
-                decreaseTrueLock(result);
-            } else {
-                decreaseFalseLock(result);
-            }
-        }
-
-        return result.get();
-    }
-
-    public synchronized void increaseTrueLock(AtomicInteger value) {
-        while (toggle) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            }
-        }
-        toggle = true;
-        notifyAll();
-        logger.info("value: " + value.incrementAndGet());
-    }
-
-    public synchronized void increaseFalseLock(AtomicInteger value) {
-        while (!toggle) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            }
-        }
-        toggle = false;
-        notifyAll();
-        logger.info("value: " + value.incrementAndGet());
-    }
-
-    public synchronized void decreaseTrueLock(AtomicInteger value) {
-        while (toggle) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            }
-        }
-        toggle = true;
-        notifyAll();
-        logger.info("value: " + value.decrementAndGet());
-
-    }
-
-
-    public synchronized void decreaseFalseLock(AtomicInteger value) {
-        while (!toggle) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
-            }
-        }
-        toggle = false;
-        notifyAll();
-        logger.info("value: " + value.decrementAndGet());
     }
 }
